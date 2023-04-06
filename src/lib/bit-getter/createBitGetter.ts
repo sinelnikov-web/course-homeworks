33bb8f53821515d826e5a26026b049262aa881a7
Максим <sinelnikov-web@mail.ru>
2023-04-06 12:36:10 +0200

    add assertion function

--- src/lib/bit-getter/createBitGetter.ts
+++ src/lib/bit-getter/createBitGetter.ts
@@ -9,24 +9,26 @@ export function createBitGetter(array: Uint8Array): BitGetter {
         array[elementIndex] = array[elementIndex] | (0b1 << bitIndex);
     }
 
+    const assertValidation = (elementIndex: number, bitIndex: number) => {
+        if (elementIndex >= array.length || elementIndex < 0) {
+            throw new Error('Array index out of range!');
+        }
+        if (bitIndex > 0b111) {
+            throw new Error('The bit index must be less than or equal to 7');
+        }
+        if (bitIndex < 0b0) {
+            throw new Error('The bit index must be more than or equal to 0')
+        }
+    }
+
     return {
         get(elementIndex: number, bitIndex: number): number {
-            if (elementIndex >= array.length) {
-                throw new Error('Array index out of range!');
-            }
-            if (bitIndex > 0b111) {
-                throw new Error('The bit index must be less than or equal to 7');
-            }
+            assertValidation(elementIndex, bitIndex);
             const bit = array[elementIndex] & (0b1 << bitIndex);
             return bit === 0 ? 0 : 1;
         },
         set(elementIndex: number, bitIndex: number, newBitValue: number): void {
-            if (elementIndex >= array.length) {
-                throw new Error('Array index out of range!');
-            }
-            if (bitIndex > 0b111) {
-                throw new Error('The bit index must be less than or equal to 7');
-            }
+            assertValidation(elementIndex, bitIndex);
             if (newBitValue === 0) {
                 resetBit(elementIndex, bitIndex);
             } else {
