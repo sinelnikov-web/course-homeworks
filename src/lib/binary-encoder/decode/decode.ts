17dbb604ffa7a5c75ceb572d9034b0817a0b31fc
Максим <sinelnikov-web@mail.ru>
2023-04-16 14:02:12 +0600

    refactor decode

--- src/lib/binary-encoder/decode/decode.ts
+++ src/lib/binary-encoder/decode/decode.ts
@@ -1,13 +1,17 @@
 import {AllowedTypes, Schema} from "../types";
 import {createBitGetter} from "../../bit-getter";
 import {BYTE_SIZE} from "../consts";
+import {convertValueByType} from "../helpers";
 
 export function decode(buffer: ArrayBuffer, schema: Schema): Array<AllowedTypes> {
+    const result: AllowedTypes[] = new Array(schema.length).fill(0) as AllowedTypes[];
+
     const view = new Uint8Array(buffer);
     const accessor = createBitGetter(view);
-    const result: AllowedTypes[] = new Array(schema.length).fill(0) as AllowedTypes[];
+
     let offset = 0;
     let currentElement = 0;
+
     for (let [size, type] of schema) {
         let value = '';
         for (let i = 0; i < size; i++) {
@@ -15,30 +19,9 @@ export function decode(buffer: ArrayBuffer, schema: Schema): Array<AllowedTypes>
             const bitIndex = 7 - ((offset + i) % BYTE_SIZE);
             value += accessor.get(byteIndex, bitIndex).toString();
         }
-        switch (type) {
-            case "number": {
-                result[currentElement] = parseInt(value, 2);
-                break;
-            }
-            case "boolean": {
-                result[currentElement] = Boolean(parseInt(value, 2));
-                break;
-            }
-            case "ascii": {
-                let str = '';
-                for (let i = 0; i < Math.ceil(size / BYTE_SIZE); i++) {
-                    const charCode = parseInt(value.slice(BYTE_SIZE * i, BYTE_SIZE * (i + 1)), 2)
-                    if (charCode !== 0) {
-                        str += String.fromCharCode(charCode);
-                    }
-                }
-                result[currentElement] = str;
-                break;
-            }
-            default: {
-                throw new Error(`TypeError: type ${type} is not assignable to types number, boolean, ascii.`)
-            }
-        }
+
+        result[currentElement] = convertValueByType(value, type, size);
+
         currentElement++;
         offset += size;
     }
--- src/lib/binary-encoder/helpers.ts
+++ src/lib/binary-encoder/helpers.ts
@@ -1,3 +1,6 @@
+import {SchemaDataType} from "./types";
+import {BYTE_SIZE} from "./consts";
+
 export function cutValue(value: any, bitCount: number): number {
     return value & (2 ** 32 - 1 >>> 32 - bitCount);
 }
@@ -7,4 +10,23 @@ export function convertToNumber(value: unknown): number {
         return parseInt([...value].reduce((acc: string, char: string, index) => acc + cutValue(char.charCodeAt(0), 8).toString(2).padStart(8, '0'), ''), 2);
     }
     return Number(value);
+}
+
+export function convertValueByType(value: string, type: SchemaDataType, size: number) {
+    if (type === 'boolean') {
+        return Boolean(parseInt(value, 2));
+    }
+    if (type === 'number') {
+        return parseInt(value, 2);
+    }
+    if (type === 'ascii') {
+        let str = '';
+        for (let i = 0; i < Math.ceil(size / BYTE_SIZE); i++) {
+            const charCode = parseInt(value.slice(BYTE_SIZE * i, BYTE_SIZE * (i + 1)), 2)
+            if (charCode !== 0) {
+                str += String.fromCharCode(charCode);
+            }
+        }
+        return str;
+    }
 }
