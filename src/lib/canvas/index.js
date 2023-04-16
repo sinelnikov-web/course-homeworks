a15fb37ec42c9eade695c4feffc7fea62f668113
Максим <sinelnikov-web@mail.ru>
2023-04-16 13:38:51 +0600

    add README for canvas lib and remove js files

--- src/lib/canvas/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CanvasUtils = void 0;
-var canvasUtils_1 = require("./canvasUtils");
-Object.defineProperty(exports, "CanvasUtils", { enumerable: true, get: function () { return canvasUtils_1.CanvasUtils; } });
