9382b87fcf3af72296387acce3bbc84c8f19e675
Максим <sinelnikov-web@mail.ru>
2023-04-23 12:39:45 +0600

    remove comments

--- src/homework-2/schemaEncoder/index.js
+++ src/homework-2/schemaEncoder/index.js
@@ -1,20 +1,3 @@
-// import {decode, encode} from "../../lib/binary-encoder";
-// import {Schema} from "lib/binary-encoder/types";
-//
-// const data = [8, 3, true, false, "abc"];
-// const buffer = encode(data, schema);
-//
-// console.log(decode(
-//     buffer,
-//     [
-//         [3, "number"],
-//         [6, "number"],
-//         [1, "boolean"],
-//         [1, "boolean"],
-//         [16, "ascii"],
-//     ]
-// )); // [0, 3, true, false, 'bc'
-
 import {encode} from "../../lib/binary-encoder/index.js";
 import {decode} from "../../lib/binary-encoder/index.js";
 
